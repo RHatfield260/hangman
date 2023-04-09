9e19cb12e49756783c07a7d152925fd8f7548c52
RHatfield260 <rhatfield260@gmail.com>
2023-04-09 21:15:37 +0100

    minor change

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,8 +1,8 @@
 import random as rd
 word_list = ['Apple','Banana','Orange','Pear','Grape']
 word = rd.choice(word_list)
-guess = input('Guess a Letter:')
-guess = guess.strip()
+guess = input('Guess a Letter:').strip()
+
 while len(guess) != 1:
-    guess = input('Oops! That is not a valid input! Please guess a single letter:')
+    guess = input('Oops! That is not a valid input! Please guess a single letter:').strip()
 print('Good Guess!')
